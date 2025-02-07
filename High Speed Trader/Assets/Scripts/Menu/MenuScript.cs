cd2581c2494e6780c123ea83b26cc7a5bdf76c75
flebbis <Samuelbas9@gmail.com>
2025-02-07 14:03:01 +0100

    Started with Menu
    Made the main menu with start and quit. Started on workign on level selctor. Geometry dash esc?

---
+++ High Speed Trader/Assets/Scripts/Menu/MenuScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // Add me!!
+
+public class MenuScript : MonoBehaviour
+{
+
+    
+    public void OnPlayButton()
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+        SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(1));
+
+    }
+    
+    public void OnQuitButton()
+    {
+        Application.Quit();
+    }
+    
+}
