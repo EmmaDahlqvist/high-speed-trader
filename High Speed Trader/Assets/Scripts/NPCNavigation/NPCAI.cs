a3e56f230b81e2860aa0acad1d221577575b1d44
flebbis <Samuelbas9@gmail.com>
2025-02-21 21:52:42 +0100

    En funkar en inte

--- High Speed Trader/Assets/Scripts/NPCNavigation/NPCAI.cs
+++ High Speed Trader/Assets/Scripts/NPCNavigation/NPCAI.cs
@@ -8,6 +8,7 @@ public class NPCAI : MonoBehaviour
 {
     GameObject[] goalLocations;
     NavMeshAgent agent;
+    Animator childAnimator;
 
     [SerializeField] private float minIdleTime = 0f;
     [SerializeField] private float maxIdleTime = 5f;
@@ -18,7 +19,26 @@ public class NPCAI : MonoBehaviour
     void Start()
     {
         agent = this.GetComponent<NavMeshAgent>();
+        if (agent == null)
+        {
+            Debug.LogError("NavMeshAgent component not found on " + gameObject.name);
+            return;
+        }
+
         goalLocations = GameObject.FindGameObjectsWithTag("NPCWanderGoal");
+        if (goalLocations.Length == 0)
+        {
+            Debug.LogError("No goal locations found with tag 'NPCWanderGoal'");
+            return;
+        }
+
+        childAnimator = transform.Find("Body")?.GetComponent<Animator>();
+        if (childAnimator == null)
+        {
+            Debug.LogError("Animator component not found on child object 'WorkerAnimated'");
+            return;
+        }
+        childAnimator.SetBool("isIdle", false);
         GoToNewLocation();
     }
 
@@ -33,14 +53,17 @@ public class NPCAI : MonoBehaviour
     private IEnumerator IdleAndMove()
     {
         isIdle = true;
+        childAnimator.SetBool("isIdle", true);
         agent.isStopped = true;
 
         float idleTime = Random.Range(minIdleTime, maxIdleTime);
         yield return new WaitForSeconds(idleTime);
 
+        childAnimator.SetBool("isIdle", false);
         agent.isStopped = false;
         GoToNewLocation();
         isIdle = false;
+
     }
 
     private void GoToNewLocation()
